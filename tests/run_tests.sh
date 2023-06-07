099f6ff7cf9473c5824509df29ef305c9c5b6278
Tim Welch <tim.j.welch@gmail.com>
2023-06-07 13:13:25 -0700

    update test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,5 +5,8 @@ echo "proj" $(proj 2>&1 | head -n 1)
 echo "geos" $(geos-config --version)
 gdalinfo --version
 ogrinfo --formats | grep FlatGeobuf
+python --version
+echo "node" $(node -v)
+echo "npm" $(npm -v)
 
 exit 0
