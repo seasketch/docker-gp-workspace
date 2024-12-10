d9d9d4bad715c2be0514cf081154f0e379a80c54
Tim Welch <tim.j.welch@gmail.com>
2024-12-09 18:23:53 -0800

    upgrade to ubuntu noble upgrade to Node v22 LTS add conda-libmamba-solver archive library error workaround pin to last miniconda release to avoid sqlite shared library not found error on gdal install test script still not running in container

--- installer/linux/amd64.sh
+++ installer/linux/amd64.sh
@@ -3,14 +3,15 @@
 # Put installer in current user home directory, install to home directory, add to user bash config, and remove installer
 mkdir -p ~/miniconda \
     && cd ~/miniconda \
-    && wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    && wget https://repo.anaconda.com/miniconda/Miniconda3-py312_24.7.1-0-Linux-x86_64.sh \
     && mkdir ~/.conda \
-    && chmod 755 Miniconda3-latest-Linux-x86_64.sh \
-    && bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/vscode/miniconda3 \
+    && chmod 755 Miniconda3-py312_24.7.1-0-Linux-x86_64.sh \
+    && bash Miniconda3-py312_24.7.1-0-Linux-x86_64.sh -b -p /home/vscode/miniconda3 \
     && echo "Running $(conda --version)" && \
     conda init bash && \
     . /home/vscode/.bashrc && \
     conda update conda && \
+    conda install --solver=classic conda-forge::conda-libmamba-solver conda-forge::libmamba conda-forge::libmambapy conda-forge::libarchive && \
     conda install python pip && \
     conda install -c conda-forge gdal && \
     cd ~ && \
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo
 # echo "proj" $(proj 2>&1 | head -n 1)
 # echo "geos" $(geos-config --version)
 gdalinfo --version
-ogrinfo --formats | grep FlatGeobuf
-python --version
-# echo "node" $(node -v)
-# echo "npm" $(npm -v)
+python3 --version
+node -v
+npm -v
 
 exit 0
